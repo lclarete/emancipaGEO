a0c89e20772fb39593b04f8f2e671093bd9b8257
Livia Clarete <livia.clarete@gmail.com>
2016-10-28 04:55:24 -0400

    scraping school name

--- emancipa.py
+++ emancipa.py
@@ -1,35 +1,65 @@
 import requests
 import lxml.html
 from bs4 import BeautifulSoup
+import urllib2
+import re
 
 urlMain = 'http://redeemancipa.org.br/institucional/quem-somos/'
 
-url_request = requests.get(urlMain)
-html = url_request.content
-soupMain = BeautifulSoup(html, 'lxml')
+urlMain_request = requests.get(urlMain)
+htmlMain = urlMain_request.content
+soupMain = BeautifulSoup(htmlMain, 'lxml')
 
-listPages = listTitle = listMail1 = listMail2 = listLocation1 = listLocation2 = []
+listURLs = []
+listTitle = []
+listMail1 = []
+listMail2 = []
+listLocation1 = []
+listLocation2 = []
 
-#Extract the URL of each school from the main HTML page
+#Extract the URL of each school from the main HTML
 for i in soupMain.select('.cursList'):
-    listPages.append(i.find('a').attrs['href'])
+    schoolURL = i.find('a').attrs['href']
+    URLnumber = re.findall(r'\d+',schoolURL)
+    #Open each URLs and read it in BeautifulSoup
+    for n in URLnumber:
+        address = ('http://redeemancipa.org.br/?page_id=' + n)
+        html = urllib2.urlopen(address).read()
+        soup = BeautifulSoup(html, "lxml")
 
-for t in soup.findAll('div', {'class':'textContent'}):
-    listTitle.append(t.find('h2').text)
+        tagContent = soup.select('.textContent')
+        #Sraping the name of each school
+        for schoolName in tagContent:
+            listTitle.append(schoolName.find('h2').text)
 
-for m1 in soup.findAll('div', {'class':'menu'}):
-    listMail2.append(m1.find('p'))
+print listTitle
 
-for m2 in soup.findAll('div', {'dir':'ltr'}):
-    mail2 = m2.findAll('a')[2]
-    listMail1.append(mail2.attrs['href'])
 
-for l1 in soup.findAll('div', {'class':'blockContent'}):
-    location1 = l1.findAll('p')[1].text
-    listLocation1.append(location1)
+# for url in listURLs:
+#     url_request = urllib2.urlopen(url)
+#     soup = BeautifulSoup(url_request, "lxml")
+#     for item in soup.findAll('.textContent'):
+#         listTitle.append(item.find('h2').text)
+#
+# print listTitle
+#
+# for x in listURLs:
+#     tagContent = soup.findAll('div', {'class':'textContent'})
+#     listTitle.append(t.find('h2'))
 
-for l2 in soup.findAll('div', {'dir':'ltr'}):
-    location2 = l2.findAll('p')[4].text
-    listLocation2.append(location2)
-
-print listTitle, listMail2, listMail1, listLocation1, listLocation2
+# for m1 in soup.findAll('div', {'class':'menu'}):
+#     listMail2.append(m1.find('p'))
+#
+# for m2 in soup.findAll('div', {'dir':'ltr'}):
+#     mail2 = m2.findAll('a')[2]
+#     listMail1.append(mail2.attrs['href'])
+#
+# for l1 in soup.findAll('div', {'class':'blockContent'}):
+#     location1 = l1.findAll('p')[1].text
+#     listLocation1.append(location1)
+#
+# for l2 in soup.findAll('div', {'dir':'ltr'}):
+#     location2 = l2.findAll('p')[4].text
+#     listLocation2.append(location2)
+#
+# print listTitle, listMail2, listMail1, listLocation1, listLocation2
