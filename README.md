b28fd5540cca14d131b58713234b5d364c5fc6db
Livia Clarete <livia.clarete@gmail.com>
2016-10-30 23:25:18 -0400

    search for the tag 'meta', and print the results in a csv file

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # emancipaGEO
-# emancipaGEO
+# The main goal of this project is to build a map, based on the information of each one of the 26 Emancipa's preparatory school.
--- emancipa.py
+++ emancipa.py
@@ -1,21 +1,19 @@
+#!/usr/bin/env python
+
 import requests
 import lxml.html
 from bs4 import BeautifulSoup
 import urllib2
 import re
+# import pyexcel
 
+#Opening the main page, with contaings the school urls which will be scrapped
 urlMain = 'http://redeemancipa.org.br/institucional/quem-somos/'
-
-urlMain_request = requests.get(urlMain)
-htmlMain = urlMain_request.content
+htmlMain = requests.get(urlMain).content
 soupMain = BeautifulSoup(htmlMain, 'lxml')
 
-listURLs = []
-listTitle = []
-listMail1 = []
-listMail2 = []
-listLocation1 = []
-listLocation2 = []
+listNames = []
+listGeneralInfo = []
 
 #Extract the URL of each school from the main HTML
 for i in soupMain.select('.cursList'):
@@ -30,36 +28,19 @@ for i in soupMain.select('.cursList'):
         tagContent = soup.select('.textContent')
         #Sraping the name of each school
         for schoolName in tagContent:
-            listTitle.append(schoolName.find('h2').text)
+            listNames.append(schoolName.find('h2').text)
+        # Scrapping the general information of each school
 
-print listTitle
+        generalInfo = soup.find('meta', attrs={'property': 'og:description'})
+        listGeneralInfo.append(generalInfo['content'].strip())
 
+        regex = r"([a-zA-Z]+) (\d+)"
+        matches = re.findall(regex, "[^abc]")
+        for match in matches:
+            print "Full match: %s" % (match)
+        print matches
+        # print match
 
-# for url in listURLs:
-#     url_request = urllib2.urlopen(url)
-#     soup = BeautifulSoup(url_request, "lxml")
-#     for item in soup.findAll('.textContent'):
-#         listTitle.append(item.find('h2').text)
-#
-# print listTitle
-#
-# for x in listURLs:
-#     tagContent = soup.findAll('div', {'class':'textContent'})
-#     listTitle.append(t.find('h2'))
+        test = zip(listNames, listGeneralInfo)
 
-# for m1 in soup.findAll('div', {'class':'menu'}):
-#     listMail2.append(m1.find('p'))
-#
-# for m2 in soup.findAll('div', {'dir':'ltr'}):
-#     mail2 = m2.findAll('a')[2]
-#     listMail1.append(mail2.attrs['href'])
-#
-# for l1 in soup.findAll('div', {'class':'blockContent'}):
-#     location1 = l1.findAll('p')[1].text
-#     listLocation1.append(location1)
-#
-# for l2 in soup.findAll('div', {'dir':'ltr'}):
-#     location2 = l2.findAll('p')[4].text
-#     listLocation2.append(location2)
-#
-# print listTitle, listMail2, listMail1, listLocation1, listLocation2
+# exportData = pyexcel.save_as(array = test, dest_file_name = "teste.csv")
